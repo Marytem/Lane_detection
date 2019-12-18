41106a71614aab12f1fbcc2ad5a87da3e00be9a1
Maryana Temnyk <temnyk@ucu.edu.ua>
2019-12-18 17:21:48 +0200

    separate metrics to other file

---
+++ metrics.py
@@ -0,0 +1,15 @@
+from keras import backend as K
+import tensorflow as tf
+
+
+def focal_loss(y_true, y_pred, gamma=2., alpha=.25):
+    pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+    pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+    return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
+
+
+def dice_coef(y_true, y_pred, smooth=1):
+    intersection = K.sum(y_true * y_pred, axis=[1,2,3])
+    union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3])
+    dice = K.mean((2. * intersection + smooth)/(union + smooth), axis=0)
+    return dice
